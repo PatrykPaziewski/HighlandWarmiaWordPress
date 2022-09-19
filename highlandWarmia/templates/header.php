abfe3c7b30d2010bd9826666c3bfb698d0a0cd82
Michal <mkrol2k@gmail.com>
2022-09-19 14:08:43 +0200

    desktop-mobile header

--- highlandWarmia/templates/header.php
+++ highlandWarmia/templates/header.php
@@ -3,3 +3,12 @@
     <img class="site-logo header-image" src="<?php echo (get_theme_file_uri("/assets/images/site-logo.png")); ?>" />
 	<?php wp_nav_menu(array('container_class' => 'menu-header', 'theme_location' => 'headerRight')); ?>
 </div>
+<div id="mobile-header" class="mobile-header">
+    <div id="menuToggle">
+        <input type="checkbox" />
+        <ul id="menu">
+            <?php wp_nav_menu(array('container_class' => 'menu-header', 'theme_location' => 'HeaderMobile')); ?>
+        </ul>
+    </div>
+    <img class="site-logo header-image" src="<?php echo (get_theme_file_uri("/assets/images/site-logo.png")); ?>" />
+</div>
